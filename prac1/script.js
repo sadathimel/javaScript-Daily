95a0e873a2a7d149c309502480b96091c4025646
Sadat Hossen Himel <40139304+sadathimel@users.noreply.github.com>
2020-02-02 10:57:46 +0600

    Add files via upload

---
+++ prac1/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("what is your last name?");
+var age = prompt("what is age?");
+alert("your full name :" + firstName + " " + lastName);
+alert("your age :" +age);
+var fullName = firstName + " " +lastName
+console.log ("Your full name is :" +fullName);
+console.log ("You are :" + age + " years old" );

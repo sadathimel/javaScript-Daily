b70ca5d06a6991ae8ebddc4bdfb051e6ebedc951
sadat <sadat.himel@gmail.com>
2020-11-02 20:00:01 +0600

    recap var const and let

--- learnWithSumit/varLetConst/vlc.js
+++ learnWithSumit/varLetConst/vlc.js
@@ -1,33 +1,64 @@
-var varVariable = "This is var";
+// var varVariable = "This is var";
 
-console.log(varVariable);
-if (true) {
-  var varVariable = "This is var again";
+// console.log(varVariable);
+// if (true) {
+//   var varVariable = "This is var again";
+// }
+
+// console.log(varVariable);
+
+// if (true) {
+//   let letVariable = "This is let";
+//   letVariable = "This is let agin";
+//   console.log(letVariable);
+// }
+
+// if (true) {
+//   const constVariable = {
+//     name: "JavaScript",
+//     age: "25 years",
+//   };
+//   constVariable.name = "js";
+//   console.log(constVariable);
+// }
+
+// var a = 10;
+// function f() {
+//   // var a = 5;
+//   console.log(a);
+// }
+// console.log(a);
+// f();
+
+
+if(true){
+  var varVariable = 'This is var';
+  var varVariable = 'This is var again';
 }
-
 console.log(varVariable);
 
-if (true) {
-  let letVariable = "This is let";
-  letVariable = "This is let agin";
+if(true){
+  let letVariable = 'This is let';
+  letVariable = 'This is let again'
   console.log(letVariable);
 }
 
-if (true) {
-  const constVariable = {
-    name: "JavaScript",
-    age: "25 years",
-  };
-  constVariable.name = "js";
-  console.log(constVariable);
+
+if(true){
+//   const constVariable = 'This is constVariable';
+//  constVariable = 'This is constVariable again';
+const constVariable = {
+  name: 'javaScript',
+  age: '25 years'
+}
+constVariable.name = "js"
+console.log(constVariable);
 }
 
-var a = 10;
-function f() {
-  // var a = 5;
-  console.log(a);
+var a = 6;
+function f(){
+  
+console.log(a)
 }
-console.log(a);
+console.log(a)
 f();
-
-

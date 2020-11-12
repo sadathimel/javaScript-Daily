06583897044b7a294b3c702a8f66ef26818aa970
sadat <sadat.himel@gmail.com>
2020-11-12 14:02:21 +0600

    name convention

--- learnWithNayem/chapter_two/variable.js
+++ learnWithNayem/chapter_two/variable.js
@@ -8,3 +8,10 @@ console.log(name + ' knows javaScript.')
 console.log('His age is ' + age);
 console.log(name + ' is creating a javaScript Course for All');
 console.log('But his age is only ' + age);
+
+
+var nameFirst = 'sadat'
+var nameLast = 'himel'
+var mathNumber = 10;
+var accountNumberDetailsId = 104;
+var account_number_details_id = 1002;

e2aedea215829ccd887e99f2c136d5a84ba07fc4
sadat <sadat.himel@gmail.com>
2020-11-12 12:56:34 +0600

    variable

---
+++ learnWithNayem/chapter_two/variable.js
@@ -0,0 +1,10 @@
+var name = 'Sadat Hossen Himel'
+var age = 23
+
+console.log(name)
+console.log(age)
+
+console.log(name + ' knows javaScript.')
+console.log('His age is ' + age);
+console.log(name + ' is creating a javaScript Course for All');
+console.log('But his age is only ' + age);

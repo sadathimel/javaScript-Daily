6e9f443d177a34204e395f95fba2a1506daabd11
sadat <sadat.himel@gmail.com>
2020-11-12 21:19:06 +0600

    data types

--- learnWithNayem/chapter_two/data_type.js
+++ learnWithNayem/chapter_two/data_type.js
@@ -14,15 +14,44 @@
 
 // *************Number*************
 
-var n = 12345
-var f = 3.14
-var nn = Number('45.45')
+// var n = 12345
+// var f = 3.14
+// var nn = Number('45.45')
 
-// console.log(nn)
-console.log(Number.parseFloat(nn))
-console.log(Number.parseInt(nn))
-console.log(Number.MAX_VALUE)
-console.log(Number.MIN_SAFE_INTEGER)
+// // console.log(nn)
+// console.log(Number.parseFloat(nn))
+// console.log(Number.parseInt(nn))
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_SAFE_INTEGER)
 
-console.log(1/0)
-console.log('abc' * 10)
+// console.log(1/0)
+// console.log('abc' * 10)
+
+// // *************String*************
+
+// var str = 'string'
+// var str2 = "string"
+// var str3 = `String`
+
+// var str4 = String('sadat himel')
+// var str5 = String(1234)
+// var str6 = String(3.1416)
+
+// console.log(str4,str5,str6);
+
+// *************Boolean*************
+// var b1 = true
+// var b2 = false
+
+// var b3 = Boolean(true)
+// var b4 = Boolean(false)
+
+// console.log(b1,b2,b3,b4)
+
+// *************Null vs undefined*************
+
+var abc
+var xyz = null 
+
+console.log(abc)
+console.log(xyz)

ff8ee9fcdc3e4bfdb50fa59040db446ac429929b
sadat <sadat.himel@gmail.com>
2020-11-12 14:18:47 +0600

    Number variable

---
+++ learnWithNayem/chapter_two/data_type.js
@@ -0,0 +1,28 @@
+// primitive
+// Number
+// String
+// Boolean
+// undefined
+// null
+
+
+// ################object###############
+// Array
+// Object
+// Function
+
+
+// *************Number*************
+
+var n = 12345
+var f = 3.14
+var nn = Number('45.45')
+
+// console.log(nn)
+console.log(Number.parseFloat(nn))
+console.log(Number.parseInt(nn))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+
+console.log(1/0)
+console.log('abc' * 10)

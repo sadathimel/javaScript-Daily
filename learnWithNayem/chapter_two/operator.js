c30c08cbcce282ea200770e093f66d2d25c32b8b
sadat <sadat.himel@gmail.com>
2020-11-14 20:14:51 +0600

    Operator

---
+++ learnWithNayem/chapter_two/operator.js
@@ -0,0 +1,71 @@
+// Arithmetic Operator
+// + - / % ++ --
+
+// var a = 10 
+// var b = 3
+
+// console.log(a % b)
+
+//  incremental
+// Pre Incremental - Post Incremental 
+
+// console.log(a++)
+// console.log(a)
+// console.log(a++)
+// console.log(a)
+// console.log(++a)
+// console.log(a++)
+// console.log(a)
+
+
+// // Assignment Operator
+// var a = 10;
+// var b = 20;
+
+// a += b;
+// console.log(a)
+
+// a -= b;
+// console.log(a)
+
+// a *= b;
+// console.log(a)
+
+// a /= b
+// console.log(a)
+
+// a %= b 
+// console.log(a)
+
+
+
+// Comparison Operator
+
+// var a = 10
+// var b = 20
+
+// console.log(a == b)
+// console.log(a != b)
+
+// console.log(a > b)
+// console.log(a < b)
+
+// a = 10
+// console.log( a >= b)
+// console.log(a <= b)
+
+// var c = '50'
+// var d = 50
+// console.log(c == d)
+// console.log(a === d)
+// console.log(c !== d)
+// console.log(c != d)
+
+
+// Logical Operator
+// &&
+// ||
+// !
+
+console.log(typeof 10)
+console.log(typeof 'himel')

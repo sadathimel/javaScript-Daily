bfdfcd9c6405167b1e5916060235775e2f37a342
sadat <sadat.himel@gmail.com>
2020-11-14 21:04:32 +0600

    math object

---
+++ learnWithNayem/chapter_two/math.js
@@ -0,0 +1,17 @@
+// console.log(Math.E)
+// console.log(Math.PI)
+
+
+var n = 4.5;
+console.log(Math.abs(n));
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.round(n));
+console.log(Math.max(400,500))
+console.log(Math.min(400,500,8,9,4,8,7,6));
+console.log(Math.pow(2,3))
+console.log(Math.pow(3,4))
+console.log(Math.sqrt(9))
+console.log(Math.sqrt(64))
+
+console.log(Math.round(Math.random() * 50 + 1))
---
+++ learnWithNayem/chapter_two/statements.js
@@ -0,0 +1,3 @@
+console.log('Hello World');
+var str = 'string'
+var number = 10 + (30 / 5) * 5

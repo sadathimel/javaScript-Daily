989ccb287b800888aa70189797e2c698d11774f6
sadat <sadat.himel@gmail.com>
2020-11-18 23:40:30 +0600

    chapter two condition operator

---
+++ learnWithNayem/chapter_three/else.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = 2;
+
+if(a > b){
+    console.log('A is greater than B')
+} else {
+    console.log('B is greater than A')
+}
+
+
+
+var n = 5;
+
+if(n % 2 === 0){
+    console.log(n + ' is Even Number')
+}else {
+    console.log(n + ' is Odd Number')
+}
---
+++ learnWithNayem/chapter_three/elseif.js
@@ -0,0 +1,22 @@
+var a = 10;
+var b = 10;
+
+if(a > b){
+    console.log('A is greater than B')
+} else if(a < b) {
+    console.log('B is greater than A')
+} else {
+    console.log('They Both are Same')
+}
+
+
+
+var n = 0;
+if( n === 0){
+    console.log(n + ' is zero')
+}
+else if(n % 2 === 0){
+    console.log(n + ' is Even Number')
+}else {
+    console.log(n + ' is Odd Number')
+}

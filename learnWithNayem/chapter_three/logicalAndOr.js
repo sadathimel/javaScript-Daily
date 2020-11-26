187256a1722e00de466e33f4cb39ba24ede86fe0
sadat <sadat.himel@gmail.com>
2020-11-26 15:20:14 +0600

    logical And Or operator

---
+++ learnWithNayem/chapter_three/logicalAndOr.js
@@ -0,0 +1,17 @@
+// var a = '';
+
+// var value = ''
+// if(a.length == 0){
+//     value = 10;
+// }else{
+//     value = a
+// }
+// console.log(value)
+
+// var name = '';
+
+// var fullName = name || 'sadat himel';
+// console.log(fullName);
+
+var isOk = false;
+isOk && console.log('everything is ok')

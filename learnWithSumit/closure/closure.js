05469b5a9ca420b50ebac4d41afa471db21ff063
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-15 17:50:14 +0600

    create toDoList

---
+++ learnWithSumit/closure/closure.js
@@ -0,0 +1,12 @@
+// var num1 = 2;
+
+// var sum = function() {
+//     var num2 = 3;
+//     return function(){
+//         return num1 + num2;
+//     }
+// };
+// var myFunc = sum();
+// console.dir(myFunc)
+
+function bankAccount()

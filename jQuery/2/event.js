f773351c86e54536c7cbde609ee4378043a0da9c
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-25 12:51:19 +0600

    practice with some events listener

--- jQuery/2/event.js
+++ jQuery/2/event.js
@@ -2,3 +2,22 @@ $("button").click(function(){
     var text = $(this).text();
 console.log("You clicked " + text);
 });
+
+$("button").on("mouseenter", function(){
+    $(this).css("font-weight","bold");
+})
+
+$("button").on("mouseleave", function(){
+    $(this).css("font-weight","normal");
+})
+
+$("input").on("keypress",function(){
+    console.log("keypressed!");
+});
+
+$("h1").on("click", function(){
+    $("h1").css("color","red");
+});
+$("h1").on("click", function(){
+    $(this).css("color","blue");
+});

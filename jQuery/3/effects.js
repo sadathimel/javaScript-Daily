f49ce7a872da418b0d6009cb348c3ca0501c8f46
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-25 14:13:14 +0600

    jQuery effects
    fadeOut ,fadeIn,slideDown,slideUp function practice

---
+++ jQuery/3/effects.js
@@ -0,0 +1,35 @@
+//// This is a fadeOut function
+
+// $("button").on("click", function(){
+//     $("div").fadeOut(1000, function(){
+//         console.log("Fade Completed!");
+//     });
+// });
+
+//// This is a fadeOut function
+
+// $("button").on("click", function(){
+//     $("div").fadeIn(1000, function(){
+
+//     })
+// })
+
+// // This is fadeToggle function
+// $("button").on("click", function(){
+//     $('div').fadeToggle(500);
+// });
+
+// // slideDown function
+// $("button").on("click", function(){
+//     $("div").slideDown();
+// })
+
+// // slideUp
+// $("button").on("click", function(){
+//     $("div").slideUp();
+// })
+
+// // slideToggle function
+// $("button").on("click", function(){
+//     $("div").slideToggle();
+// })

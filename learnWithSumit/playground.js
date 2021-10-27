3ecf363ccf0fce38a237d42255beb35b1e445981
Mahadi001 <sadat.himel@gmail.com>
2021-10-28 01:09:37 +0600

    practice with global this and object this

--- learnWithSumit/playground.js
+++ learnWithSumit/playground.js
@@ -8,9 +8,17 @@
 var javaScript = {
     name : "JavaScript",
     libraries: ["React", "Angular", "vue"],
+   
     printLibraries: function () {
+        //use object this
+        var self = this;
+        // console.log(this);
         this.libraries.forEach(function(e){
-            console.log(`${this.name} loves ${e}`);
+            // console.log(this) //this forEach change this valu
+            // console.log(`${this.name} loves ${e}`);
+
+            // now this is a object this
+            console.log(`${self.name} loves ${e}`);
         })
     }
 }

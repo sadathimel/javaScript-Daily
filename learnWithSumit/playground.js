c97b8c93c73f401dd6f8489b76241831a07b5a8d
Mahadi001 <sadat.himel@gmail.com>
2021-10-28 01:14:32 +0600

    arrow function this

--- learnWithSumit/playground.js
+++ learnWithSumit/playground.js
@@ -11,15 +11,18 @@ var javaScript = {
    
     printLibraries: function () {
         //use object this
-        var self = this;
+        // var self = this;
         // console.log(this);
-        this.libraries.forEach(function(e){
+        // this.libraries.forEach(function(e){
             // console.log(this) //this forEach change this valu
             // console.log(`${this.name} loves ${e}`);
 
             // now this is a object this
-            console.log(`${self.name} loves ${e}`);
-        })
+            // console.log(`${self.name} loves ${e}`);
+        // })
+
+        // arrow function don't change this value
+        this.libraries.forEach((e)=> console.log(`${this.name} loves ${e}`))
     }
 }
 

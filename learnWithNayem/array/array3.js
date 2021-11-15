05eea96dadd69c6751eb30af2e2dbac85384a775
Mahadi001 <sadat.himel@gmail.com>
2021-11-15 14:02:15 +0600

    updated

--- learnWithNayem/array/array3.js
+++ learnWithNayem/array/array3.js
@@ -2,18 +2,17 @@
 // SIMPLE TRAVERSE ARRY ELEMENTS
 
 // for (let i = 0; i < arr.length; i++){
-// 	console.log(arr[i]); 
+// 	console.log(arr[i]);
 // }
 
-
-// sum array elements 
-// 
+// sum array elements
+//
 // let sum = 0;
 // for(let i = 0; i < arr.length; i++){
 // 	sum = sum + arr[i];
-// } 
+// }
 
-// console.log(sum); 
+// console.log(sum);
 // console.log(sum / arr.length);
 
 // find lergest number
@@ -24,23 +23,23 @@
 // 		big = arr[i];
 // 	}
 // }
-// console.log(big); 
-// 
+// console.log(big);
+//
 
-// find small number 
+// find small number
 
 // let small = arr[0];
 // for(var i = 1; i < arr.length; i++){
 //  	if(arr[i] < small){
 //  		small = arr[i];
 //  	}
-//  } 
+//  }
 
 //  console.log(small);
 
 //  console.log(Math.max(...arr));
 
-const arr = [600,42,63,4,554];
+// const arr = [600,42,63,4,554];
 
 // let big = arr[0];
 // for(let i = 0; i < arr.length; i++){
@@ -48,44 +47,30 @@ const arr = [600,42,63,4,554];
 // 		big = arr[i];
 // 	}
 // }
-// console.log(big); 
+// console.log(big);
 
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+// 	sum = sum + arr[i];
+// 	sum = sum / arr.length;
+// }
+// console.log(sum)
 
-let sum = 0;
-for(let i = 0; i < arr.length; i++){
-	sum = sum + arr[i];
-	sum = sum / arr.length;
-} 
-console.log(sum)
+const arr = [600, 42, 63, 4, 554];
 
+// let big = arr[0];
 
+// for (let i = 0; i < arr.length; i++) {
+//   if (big < arr[i]) {
+//     big = arr[i];
+//   }
+// }
+// console.log(big);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+// 	sum = sum + arr[i];
+// 	sum = sum / arr.length
+	
+// }
+// console.log(sum);

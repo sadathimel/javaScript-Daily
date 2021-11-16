39311fcce7e1f549a61e0a690126ee1b575bca49
Mahadi001 <sadat.himel@gmail.com>
2021-11-16 13:43:09 +0600

    array concat

---
+++ learnWithNayem/array/array-crud/array4.js
@@ -0,0 +1,9 @@
+// Concat Multiple Arrays
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 = arr1.concat(arr2);
+// console.log(arr3)
+
+const arr5 = [...arr1, ...arr2];
+console.log(arr5)

f9a0ace068dd0d39ef53c9018844c9884d96b818
Mahadi001 <sadat.himel@gmail.com>
2021-11-16 13:45:21 +0600

    array concate

--- learnWithNayem/array/array-crud/array4.js
+++ learnWithNayem/array/array-crud/array4.js
@@ -5,5 +5,8 @@ const arr2 = [4,5,6];
 const arr3 = arr1.concat(arr2);
 // console.log(arr3)
 
-const arr5 = [...arr1, ...arr2];
-console.log(arr5)
+const arr4 = [...arr1, ...arr2];
+// console.log(arr5)
+
+const arr5 = [].concat(arr1,arr2, 7,8,arr4);
+console.log(arr5);

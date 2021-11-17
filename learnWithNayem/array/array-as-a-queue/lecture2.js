73ff88cc1b1c3882f76da0c7a6c2fa4231ef5c09
Mahadi001 <sadat.himel@gmail.com>
2021-11-17 16:19:03 +0600

    array object method push and pop

--- learnWithNayem/array/array-as-a-queue/lecture2.js
+++ learnWithNayem/array/array-as-a-queue/lecture2.js
@@ -1,9 +1,19 @@
 const Queue = require('./lecture1');
 
-const queue = new Queue();
-queue.enqueue('Task One');
-queue.enqueue('Task Two');
-queue.enqueue('Task Three');
-queue.showQueue();
-queue.dequeue();
-queue.showQueue();
+// const queue = new Queue();
+// queue.enqueue('Task One');
+// queue.enqueue('Task Two');
+// queue.enqueue('Task Three');
+// queue.showQueue();
+// queue.dequeue();
+// queue.showQueue();
+
+
+const q = [];
+q.push('Item One');
+q.push('Item Two');
+q.push('Item Three');
+console.log(q);
+q.shift();
+q.shift();
+console.log(q);

18bed466b87b1cf1421f2cd94f6aed8d83ff04c6
Mahadi001 <sadat.himel@gmail.com>
2021-11-18 12:58:13 +0600

    filter

---
+++ learnWithNayem/array/iterating-array-elements/lecture3.js
@@ -0,0 +1,17 @@
+const studentArray = [
+  { id: 1, name: "Sadat Himel", gpa: 3.99, email: "sadat.himel@gmail.com" ,due:true, dueAmount:3000 },
+  { id: 2, name: "Tanvir ahmed opel", gpa: 3.87, email: "tanvir@gmail.com",due:false, dueAmount:0 },
+  { id: 3, name: "Saidul hasan", gpa: 2.09, email: "saidulhasan@gmail.com",due:true, dueAmount:12000 },
+  { id: 4, name: "pronoy sorkar", gpa: 3.8, email: "pronoysorkar@gmail.com",due:false, dueAmount:0 },
+  { id: 5, name: "Sheik Nazmul", gpa: 2.19, email: "nazmulsheik@gmail.com" ,due:false, dueAmount:0},
+];
+
+// const lowGpa = studentArray.filter((value)=>value.gpa < 3);
+// const lowGpa = studentArray.filter((value)=>{
+//   if(value.dueAmount  > 0 ){
+//     return{
+//         ...value,
+//         inspiringMessage: `hi, ${value.name} your result ${value.dueAmount > 0 ? "your have a due" : "thanks you don't due"} ` 
+//     };
+// }})
+// console.log(lowGpa);

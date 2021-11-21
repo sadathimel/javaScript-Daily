769ab1c66365a333ce32590b970095c4673c2e80
Mahadi001 <sadat.himel@gmail.com>
2021-11-21 16:47:02 +0600

    updated

--- learnWithNayem/array/reduce-method/lecture17.js
+++ learnWithNayem/array/reduce-method/lecture17.js
@@ -12,3 +12,13 @@ const ids = [1,2,3,4,5]
 // ids.forEach((v)=>{
 //     createPromise(v);
 // })
+
+const result = ids.reduce((acc,cur)=> {
+    return acc.then(()=> {
+        return createPromise(cur);
+    })
+},Promise.resolve())
+
+result.then(()=>{
+    console.log('Done!!!');
+})

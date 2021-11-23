5a39be54033370c539e0c6ed89a2c656e8776c3d
Mahadi001 <sadat.himel@gmail.com>
2021-11-23 16:31:43 +0600

    includes,indexOf, lastIndexOf

---
+++ learnWithNayem/other-array-methods/lecture1.js
@@ -0,0 +1,8 @@
+// const arr = [];
+// arr.concat(); //Instance Method
+// Array.from(); // Static Method 
+
+var a = ['Stack', 'Lerner', 'Javascript']
+
+const join1 = a.join(', + ');
+console.log(join1)
---
+++ learnWithNayem/other-array-methods/lecture2.js
@@ -0,0 +1,6 @@
+const arr = ['One', 'Two', 'Three','One'];
+
+console.log(arr.includes('five'));
+
+console.log(arr.indexOf('One'))
+console.log(arr.lastIndexOf('One'))

3af0b6b26e951d6c82105434a72edfb87f39f9cf
Mahadi001 <sadat.himel@gmail.com>
2021-11-26 12:52:24 +0600

    forloop

---
+++ learnWithNayem/loop/forLoop.js
@@ -0,0 +1,21 @@
+// for (var i=0 ; i <= 100; i++){
+//     if(i % 2 === 1){
+//         console.log(i)
+//     }
+// }
+// let length = 10
+// let sum = 0
+// for (let i = 0; i <= length; i++) {
+//     sum += i;
+    
+// }
+// console.log(sum);
+let sum = 0;
+for(let i = 0; i <=100; i++){
+    if(i%2===1){
+
+console.log(sum + ' + '+ i + '=' +(sum+i))
+sum += i;
+    }
+}
+console.log("Result = " + sum);

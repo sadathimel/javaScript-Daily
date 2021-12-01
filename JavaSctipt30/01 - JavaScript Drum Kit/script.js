1c190c6c6d7a119bd3a0b635a249720713015536
Mahadi001 <sadat.himel@gmail.com>
2021-12-01 23:29:59 +0600

    updated

--- JavaSctipt30/01 - JavaScript Drum Kit/script.js
+++ JavaSctipt30/01 - JavaScript Drum Kit/script.js
@@ -1,18 +1,20 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    // console.log(key);
-    if(!audio)return;
-    audio.currentTime = 0;
-    audio.play();
-     key.classList.add('playing');
-});
+function playSound(e){
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   // console.log(key);
+   if(!audio)return;
+   audio.currentTime = 0;
+   audio.play();
+   key.classList.add('playing');
+   
+}
 
 function removeTransition(e){
    if(e.propertyName !== 'transform') return;
    this.classList.remove('playing');
-
 }
+
+window.addEventListener('keydown', playSound);
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend',removeTransition));

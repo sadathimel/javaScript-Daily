509066c56b3b9e965412b69d58ceafab3c79b7ce
sadathimel <sadat.himel@gmail.com>
2021-12-07 18:01:47 +0600

    updated

---
+++ .vscode/javascriptdailyproject/Animated Eyes Follow Mouse Cursor using Vanilla Javascript/script.js
@@ -0,0 +1,12 @@
+document.querySelector("body").addEventListener('mousemove', eyeball);
+
+function eyeball(){
+    var eye = document.querySelectorAll('.eye');
+    eye.forEach(function(eye){
+        let x = (eye.getBoundingClientRect().right) + (eye.clientWidth / 2);
+        let y = (eye.getBoundingClientRect().bottom) + (eye.clientHeight / 2);
+        let radian = Math.atan2(event.pageX - x, event.pageY -y);
+        let rot = (radian * (180 / Math.PI) * -1) + 270;
+        eye.style.transform = "rotate("+ rot + "deg)";
+    })
+}

8515911ac9f1d816d0035a6eee0db24fedec2e5f
sadathimel <sadat.himel@gmail.com>
2021-12-22 14:40:28 +0600

    updated

--- javascriptdailyproject/New-Year-Countdown/main.js
+++ javascriptdailyproject/New-Year-Countdown/main.js
@@ -10,5 +10,18 @@ function counter(){
             day = hour * 24;
 
         let dayLeft = Math.floor(timeLeft / day),    
-        let hourLeft = Math.floor(timeLeft / day),    
+            hourLeft = Math.floor((timeLeft % day) / hour),    
+            minuteLeft = Math.floor((timeLeft % hour) / minute),    
+            secondLeft = Math.floor((timeLeft % minute) / second);
+            
+            
+        document.getElementById('day').innerHTML = dayLeft;
+        document.getElementById('hour').innerHTML = hourLeft;
+        document.getElementById('minute').innerHTML = minuteLeft;
+        document.getElementById('second').innerHTML = secondLeft;    
 }
+setInterval(()=>{
+    
+    counter();
+})
+

5980b1c54a586d36b5a8c1c3c8371de9a3a9420d
sadathimel <sadat.himel@gmail.com>
2021-12-22 13:54:51 +0600

    updated

--- javascriptdailyproject/New-Year-Countdown/main.js
+++ javascriptdailyproject/New-Year-Countdown/main.js
@@ -3,4 +3,12 @@ var newYear = new Date('Jan 1, 2022 00:00:00').getTime();
 function counter(){
     let dateCurrent = new Date().getTime(),
         timeLeft = newYear - dateCurrent;
+    
+        let second = 1000;
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+        let dayLeft = Math.floor(timeLeft / day),    
+        let hourLeft = Math.floor(timeLeft / day),    
 }

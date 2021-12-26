ff81d9768c7dd376a8860fe26987e39502544cf3
sadathimel <sadat.himel@gmail.com>
2021-12-26 13:07:15 +0600

    updated

---
+++ javascriptdailyproject/TicTacToe/main.js
@@ -0,0 +1,17 @@
+var i1 = document.getElementById("i1");
+var i2 = document.getElementById("i2");
+var i3 = document.getElementById("i3");
+var i4 = document.getElementById("i4");
+var i5 = document.getElementById("i5");
+var i6 = document.getElementById("i6");
+var i7 = document.getElementById("i7");
+var i8 = document.getElementById("i8");
+var i9 = document.getElementById("i9");
+var p1 = document.getElementById("p1");
+
+document.getElementById('b1').onclick = function(){
+    location.reload();
+}
+
+var x = "0";
+var count = 0;

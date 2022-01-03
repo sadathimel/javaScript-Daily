78262e8dd82606cd89b5abfa5564af4bfa707cfe
sadathimel <sadat.himel@gmail.com>
2022-01-03 17:25:54 +0600

    updated

--- javascriptdailyproject/TicTacToe/main.js
+++ javascriptdailyproject/TicTacToe/main.js
@@ -8,36 +8,38 @@ var i7 = document.getElementById("i7");
 var i8 = document.getElementById("i8");
 var i9 = document.getElementById("i9");
 var p1 = document.getElementById("p1");
-
-document.getElementById('b1').onclick = function(){
+document.getElementById("b1").onclick = function() {
     location.reload();
 }
-
-var x = "0";
+var x = "O";
 var count = 0;
 
-
-
-function check(l){
-    if (x == "0") {
+function check(i) {
+    if (x == "O") {
         x = "X";
-    }else {
-        x = "0";
+    } else {
+        x = "O";
     }
     count++;
-    console.log(x)
-    if(p1.innerHTML == "" && count == 9){
+    if (p1.innerHTML == "" && count == 9) {
         p1.innerHTML = "Draw !";
         goBack();
     }
-
-    document.getElementById("i" + l).innerHTML = x;
-
-    document.getElementById("i")
-    
-console.log(i)
-
-    function goBack(){
-        b1.style.visibility = "visible"
+    console.log(count)
+    document.getElementById("i" + i).innerHTML = x;
+    document.getElementById("i" + i).style.color = "black";
+    if ((i1.innerHTML == i2.innerHTML && i2.innerHTML == i3.innerHTML) ||
+        (i4.innerHTML == i5.innerHTML && i5.innerHTML == i6.innerHTML) ||
+        (i7.innerHTML == i8.innerHTML && i8.innerHTML == i9.innerHTML) ||
+        (i1.innerHTML == i4.innerHTML && i4.innerHTML == i7.innerHTML) ||
+        (i2.innerHTML == i5.innerHTML && i5.innerHTML == i8.innerHTML) ||
+        (i3.innerHTML == i6.innerHTML && i6.innerHTML == i9.innerHTML) ||
+        (i1.innerHTML == i5.innerHTML && i5.innerHTML == i9.innerHTML) ||
+        (i3.innerHTML == i5.innerHTML && i5.innerHTML == i7.innerHTML)) {
+        p1.innerHTML = "'" + x + "' wins !";
+        goBack();
+    }
+    function goBack() {
+        b1.style.visibility = "visible";
     }
 }

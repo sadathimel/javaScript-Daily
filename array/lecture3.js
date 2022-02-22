1b57aadf1e8addee9968a6eb88345ace35df1737
sadathimel <sadat.himel@gmail.com>
2022-02-22 16:04:03 +0600

    updated

--- array/lecture3.js
+++ array/lecture3.js
@@ -3,7 +3,7 @@
  * Traversing an array
  * **/
 
-const arr = [41, 22, 73, 24, 95];
+// const arr = [41, 22, 73, 24, 95];
 
 // //simple for loop
 // let sum = 0;
@@ -24,12 +24,49 @@ const arr = [41, 22, 73, 24, 95];
 // console.log('LargestNumber = ' + largestNumber);
 
 // smallest number from array
-let smallestNumber = arr[0];
+// let smallestNumber = arr[0];
 
-for (let i = 0; i < arr.length; i++) {
-    if(arr[i] < smallestNumber){
-        smallestNumber = arr[i];
-    }
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] < smallestNumber){
+//         smallestNumber = arr[i];
+//     }
+// }
+
+// console.log('SmallestNumber = ' + smallestNumber);
+
+
+// let total = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//    total += arr[i];
+// }
+// console.log(total);
+// console.log(total / arr.length);
+
+
+// const arr1 = new Array(10);
+// for (let i = 0; i < arr1.length; i++) {
+//     arr1[i] = false;
+// }
+// // console.log(arr1);
+
+// const arr2 = new Array(10);
+// arr2 .fill(0);
+// console.log(arr2);
+
+// arr1 = [22,34,23,55];
+
+// let bigNumber = arr1[0];
+
+// for(let i = 0; i < arr1.length; i++){
+//     if(arr1[i] > bigNumber){
+//         bigNumber = arr1[i]
+//     }
+// }
+// console.log(bigNumber);
+
+let arr1 = new Array(10);
+for( i = 0; i < arr1.length; i++){
+    arr1[i] = false;
 }
-
-console.log('SmallestNumber = ' + smallestNumber);
+console.log(arr1);

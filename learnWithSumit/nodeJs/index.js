999b84644e2220703394acfd7c61c6506e61af80
sadathimel <sadat.himel@gmail.com>
2022-02-28 12:50:45 +0600

    updated

--- learnWithSumit/nodeJs/index.js
+++ learnWithSumit/nodeJs/index.js
@@ -1,3 +1,12 @@
-let a = 5;
-a = 6;
-console.log(a);
+const fs = require('fs'); // file system
+
+fs.writeFileSync('myfile.txt', 'Hello Programmers');
+// fs.appendFileSync('myfile.txt', ' Hello There', (err) => {
+//     if (err) throw err;
+//     console.log('Successfully written to file');
+// });
+
+fs.readFile('myfile.txt', (err, read) => {
+    if (err) throw err;
+    console.log(read.toString());
+});

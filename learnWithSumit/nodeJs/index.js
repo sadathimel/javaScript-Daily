8181e52624724169afc777ce95d703ce63797558
sadathimel <sadat.himel@gmail.com>
2022-02-28 12:56:12 +0600

    updated

--- learnWithSumit/nodeJs/index.js
+++ learnWithSumit/nodeJs/index.js
@@ -1,12 +1,21 @@
-const fs = require('fs'); // file system
+// const fs = require('fs'); // file system
 
-fs.writeFileSync('myfile.txt', 'Hello Programmers');
-// fs.appendFileSync('myfile.txt', ' Hello There', (err) => {
+// fs.writeFileSync('myfile.txt', 'Hello Programmers');
+// // fs.appendFileSync('myfile.txt', ' Hello There', (err) => {
+// //     if (err) throw err;
+// //     console.log('Successfully written to file');
+// // });
+
+// fs.readFile('myfile.txt', (err, read) => {
 //     if (err) throw err;
-//     console.log('Successfully written to file');
+//     console.log(read.toString());
 // });
+const EventEmitter = require('events');
 
-fs.readFile('myfile.txt', (err, read) => {
-    if (err) throw err;
-    console.log(read.toString());
+const emitter = new EventEmitter();
+
+emitter.on('message', () => {
+    console.log('Hello');
 });
+
+emitter.emit('message');

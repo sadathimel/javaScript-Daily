93363bd31c54921951651878356e436da1f83ef7
sadathimel <sadat.himel@gmail.com>
2022-05-31 13:14:41 +0600

    array of object

--- array/lecture5.js
+++ array/lecture5.js
@@ -4,3 +4,27 @@ console.log(arr)
 function getTen(){
     return 10;
 }
+
+// Array of Objects
+const favChannels = [
+    { name: 'Stack Learner', url: 'https://www.youtube.com/stacklearner'},
+    { name: 'Js Bangladesh', url: 'https://www.youtube.com/jsbangladesh'},
+    { name: 'Traversy Media', url: 'https://www.youtube.com/techguyweb'},
+    { name: 'Wes Bos', url: 'https://www.youtube.com/wesbos'},
+];
+
+// Array of Functions
+const sum = (a,b) => a + b;
+const sub = (a,b) => a - b;
+const times = (a,b) => a * b;
+const div = (a,b) => a / b;
+const mod = (a,b) => a % b;
+
+const funcs = [sum, sub, times, div, mod];
+const a = 10;
+    b = 20;
+
+    for (let i = 0; i < funcs.length; i++){
+        const result = funcs[i](a,b);
+        console.log(result);
+    }

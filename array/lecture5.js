f0cb7ca8b05633a6afbe81812d5aa4b7271350c5
sadathimel <sadat.himel@gmail.com>
2022-06-12 17:46:55 +0600

    array with matrix

--- array/lecture5.js
+++ array/lecture5.js
@@ -5,6 +5,8 @@ function getTen(){
     return 10;
 }
 
+
+
 // Array of Objects
 const favChannels = [
     { name: 'Stack Learner', url: 'https://www.youtube.com/stacklearner'},
@@ -20,13 +22,20 @@ const times = (a,b) => a * b;
 const div = (a,b) => a / b;
 const mod = (a,b) => a % b;
 
+const names = [sum, sub, times, div, mod];
+
+for(i = 0; i < names.length; i++){
+    // console.log(names[i].name)
+}
+
+
 const funcs = [sum, sub, times, div, mod];
 const a = 10;
     b = 20;
 
     for (let i = 0; i < funcs.length; i++){
         const result = funcs[i](a,b);
-        // console.log(result);
+        // console.log(`${funcs[i].name}`);
     }
     
     //Array of Arrays - Multi Dimensional Array
@@ -45,7 +54,7 @@ const a = 10;
     // Two Dimensional Traverse
     for (let i = 0; i < pointTable.length; i++){
         for(let j = 0; j < pointTable[i].length; j++){
-            console.log(`Points [${i} , ${j} ] = ${pointTable[i][j]}`);
+            // console.log(`Points [${i} , ${j} ] = ${pointTable[i][j]}`);
         }
     }
 
@@ -65,3 +74,32 @@ const a = 10;
             // console.log(numbers[i][j]);
         }
     }
+
+
+
+    // Matrix Example
+    const matrixA = [
+        [1, 3],
+        [1, 0],
+        [1, 2]
+    ]
+    const matrixB = [
+        [0, 0],
+        [7, 5],
+        [2, 1]
+    ];
+
+
+    const matrixSum = (matrixA, matrixB) => {
+        const result = [];
+        for(let i = 0; i < matrixA.length; i++){
+            const row = [];
+            for(let j = 0; j < matrixA[i].length; j++){
+                row.push(matrixA[i][j] + matrixB[i][j]); 
+            }
+            result.push(row);
+        }
+        return result;
+    }
+    const matrixC = matrixSum(matrixA, matrixB);
+    console.log(matrixC);

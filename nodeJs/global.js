ca65dddef7836e203efab2d201316243074facda
sadathimel <sadat.himel@gmail.com>
2022-08-04 18:07:55 +0600

    nodeJs module

--- nodeJs/global.js
+++ nodeJs/global.js
@@ -1,10 +1,18 @@
-console.log('Hello World');
+// console.log('Hello World');
 
 //setTimeout function
 
-setTimeout(function () {
-    console.log('i am from global');
-} , 1000);
+// setTimeout(function () {
+//     console.log('i am from global');
+// } , 1000);
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
+var a = 10;
+
+function test(){
+    console.log('i am Test');
+}
+
+
+console.log(module)
---
+++ nodeJs/math.js
@@ -0,0 +1,16 @@
+// const add = (a, b) => a + b;
+exports.add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const div = (a, b) => a / b;
+
+
+exports.test = (a, b) => add(a, b) / sub(a, b);
+// const test = (a, b) => add(a, b) / sub(a, b);
+
+// module.exports = {
+//     test,
+//     add
+// };
+// module.exports.add = add;
+// module.exports.test = test;
+console.log(module);

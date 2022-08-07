77e853a236667e8d27f94599b70d1976106c6bd8
sadathimel <sadat.himel@gmail.com>
2022-08-07 17:23:16 +0600

    path

---
+++ nodeJs/path.js
@@ -0,0 +1,19 @@
+// console.log(__filename);
+const path = require('path');
+const myCurrentPath = __filename;
+console.log(path.basename(__dirname));
+console.log(path.extname(myCurrentPath));
+
+let patObj = {
+    dir: 'usr/local',
+    name: 'textFile',
+    ext: '.js'
+}
+
+console.log(path.format(patObj));
+
+console.log(path.isAbsolute(myCurrentPath));
+
+console.log(path.isAbsolute('./math.js'));
+
+console.log(path.join('user', 'local', 'textFile.js'));

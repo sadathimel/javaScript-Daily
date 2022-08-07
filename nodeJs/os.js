23fb35d47159956628c63708cfc22e22c483f324
sadathimel <sadat.himel@gmail.com>
2022-08-08 00:57:14 +0600

    nodeJs OS module

---
+++ nodeJs/os.js
@@ -0,0 +1,14 @@
+const os = require('os');
+
+// console.log(os);
+
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.networkInterfaces());
+
+console.log(os.hostname());
+console.log(os.homedir());
+console.log(os.type());
+console.log(os.userInfo());

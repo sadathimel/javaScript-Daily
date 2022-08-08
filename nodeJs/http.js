92ac801a00e3656c191a8992c6a95a847c59136f
sadathimel <sadat.himel@gmail.com>
2022-08-08 12:46:52 +0600

    nodeJs http module

--- nodeJs/http.js
+++ nodeJs/http.js
@@ -1,4 +1,11 @@
 const http = require('http'); 
 
-// http.
-console.log(http)
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    res.end('<h1>Hello NodeJs</h1>');
+})
+// console.log(http)
+
+server.listen(4040,()=>{
+    console.log('Server in running port 4040');
+})

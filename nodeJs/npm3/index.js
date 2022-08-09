3199698792bebf2ffa22a92d15638dda10e853bb
sadathimel <sadat.himel@gmail.com>
2022-08-10 00:58:29 +0600

    how to use dependence

---
+++ nodeJs/npm3/index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+console.log(chalk.red.bold('Hello World!'));
+
+console.log(chalk.white.inverse('Another Text'));
+
+console.log("This is New Line");
+
+// output console changes color and style
+console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));

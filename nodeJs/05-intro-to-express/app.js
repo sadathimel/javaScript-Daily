37d3496d63b14cfe6941dde741e6d88aab927f3a
sadathimel <sadat.himel@gmail.com>
2022-08-10 18:24:40 +0600

    express routing

--- nodeJs/05-intro-to-express/app.js
+++ nodeJs/05-intro-to-express/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.get('/about', (req, res)=>{
+    res.send('I am A About Page');
+})
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, ()=>{

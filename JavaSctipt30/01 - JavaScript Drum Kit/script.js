d2a06fe524ad5717c910f44fea07dae079a863f5
sadathimel <sadat.himel@gmail.com>
2023-04-05 14:06:07 +0600

    updated file

--- JavaSctipt30/01 - JavaScript Drum Kit/script.js
+++ JavaSctipt30/01 - JavaScript Drum Kit/script.js
@@ -1,20 +1,42 @@
-function playSound(e){
-   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-   // console.log(key);
-   if(!audio)return;
-   audio.currentTime = 0;
-   audio.play();
-   key.classList.add('playing');
-   
+// function playSound(e){
+//    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+//    // console.log(key);
+//    if(!audio)return;
+//    audio.currentTime = 0;
+//    audio.play();
+//    key.classList.add('playing');
+
+// }
+
+// function removeTransition(e){
+//    if(e.propertyName !== 'transform') return;
+//    this.classList.remove('playing');
+// }
+
+// window.addEventListener('keydown', playSound);
+
+// const keys = document.querySelectorAll('.key');
+// keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  if (!audio) return;
+  console.log("after return statement");
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
 }
 
-function removeTransition(e){
-   if(e.propertyName !== 'transform') return;
-   this.classList.remove('playing');
+window.addEventListener("keydown", playSound);
+
+function removeTransition(e) {
+  //   console.log(e);
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 
-window.addEventListener('keydown', playSound);
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));

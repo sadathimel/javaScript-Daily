117df4138f138dd4b2f906f005845fd7d07c4691
sadathimel <sadat.himel@gmail.com>
2023-05-09 13:00:41 +0600

    updated gsap file

--- JavaSctipt30/02 - JS and CSS Clock/script.js
+++ JavaSctipt30/02 - JS and CSS Clock/script.js
@@ -1,24 +1,39 @@
- 
- const secondHand = document.querySelector('.second-hand');
- const minHand = document.querySelector('.min-hand');
- const hourHand = document.querySelector('.hour-hand');
- 
- function setDate(){
-     const now = new Date();
-     const seconds = now.getSeconds();
-     const secondsDegrees = (seconds / 60) * 360 + 90;
-     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+// const secondHand = document.querySelector(".second-hand");
+// const minHand = document.querySelector(".min-hand");
+// const hourHand = document.querySelector(".hour-hand");
 
-     const mins = now.getMinutes();
-     const minutesDegrees = ((mins / 60) * 360) + 90;
+// function setDate() {
+//   const now = new Date();
+//   const seconds = now.getSeconds();
+//   const secondsDegrees = (seconds / 60) * 360 + 90;
+//   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
-     
-     const hour = now.getHours();
-     const hoursDegrees = ((hour / 12) * 360) + 90;
+//   const mins = now.getMinutes();
+//   const minutesDegrees = (mins / 60) * 360 + 90;
 
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-     
- }
+//   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
- setInterval(setDate, 1000);
+//   const hour = now.getHours();
+//   const hoursDegrees = (hour / 12) * 360 + 90;
+
+//   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+// }
+
+// setInterval(setDate, 1000);
+
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  //   console.log(seconds);
+  //   console.log(secondsDegrees);
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const min = now.getMinutes();
+  const hour = now.getHours();
+  console.log(hour + ":" + min + ":" + seconds);
+}
+setInterval(setDate, 1000);
